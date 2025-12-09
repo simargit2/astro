c93e9720b943790551a6e5831d7234316b435977
simargit2 <simargithub2@gmail.com>
2025-12-10 00:41:13 +0530

    netlify deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // <--- ADD THIS LINE HERE
   vite: {
     plugins: [tailwindcss()]
   }
