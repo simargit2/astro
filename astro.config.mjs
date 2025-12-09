d7bcd40438932c519605e447aa2d93094570fd51
simargit2 <simargithub2@gmail.com>
2025-12-10 00:49:08 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // <--- ADD THIS LINE HERE
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
